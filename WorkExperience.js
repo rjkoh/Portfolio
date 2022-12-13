857db4c76b4a1b5942c77030119f60e0469b01f6
rjkoh <koh.ruijie123@gmail.com>
2022-12-14 02:13:55 +0800

    fix mobile layout

--- WorkExperience.js
+++ WorkExperience.js
@@ -1,8 +1,8 @@
 let display = "1";
 
-const GMC = '<p class="font-bold text-xl mb-2">Mar 2021 - Jul 2021</p><ul><li>Liasing with vendors and customers through phone and email</li><li>Ad-hoc support such as taking minutes, MS Office troubleshooting</li><li>Managed projects such as renovations, involving meeting arrangements, planning and liasing</li></ul>';
+const GMC = '<p class="font-bold sm:text-xl mb-2">Mar 2021 - Jul 2021</p><p class="font-bold sm:text-lg mb-2">Operations and Admin Assistant</p><ul class="list-disc pl-5 sm:text-base text-sm"><li>Liasing with vendors and customers through phone and email</li><li>Ad-hoc support such as taking minutes, MS Office troubleshooting</li><li>Managed projects such as renovations, involving meeting arrangements, planning and liasing</li></ul>';
 
-const SAF = '<p class="font-bold text-xl mb-2">Apr 2019 - Feb 2021</p><p class="font-bold text-lg mb-2">Battery Commander, 23rd Singapore Artillery</p><ul><li>Awarded top 30% in Officer Cadet School</li><li>Commanded a company of 100 men and served as bridge between Regular officers and NSF company</li><li>Planned a battalion-wide anniversary activity involving almost 500 people</li></ul>';
+const SAF = '<p class="font-bold sm:text-xl mb-2">Apr 2019 - Feb 2021</p><p class="font-bold sm:text-lg mb-2">Battery Commander, 23rd Singapore Artillery</p><ul class="list-disc pl-5 sm:text-base text-sm"><li>Awarded top 30% in Officer Cadet School</li><li>Commanded a company of 100 men and served as bridge between Regular officers and NSF company</li><li>Planned a battalion-wide anniversary activity involving almost 500 people</li></ul>';
 
 const details = [
     SAF,
